840d98a1caf1b00b8cbcb3ffb06cb86d689c619d
Bilal Al-Essa <bilal.al-essa@outlook.com>
2020-10-23 11:54:27 -0700

    Adding Inheritance Assignment

---
+++ Uploads/Inheritance Assignment/Init Assignment.py
@@ -0,0 +1,12 @@
+class aptTenant:
+    person_name = 'Leaving Blank'
+    address = '12345 East Random Rd, Portland Oregon 97123'
+
+class staff(aptTenant):
+    position = 'Administration'
+    base_pay = 10.00
+    
+class resident(aptTenant):
+    roomnumber = 0
+    base_rent = 1000.00
+
